3923cc2c09bf3d4f638d3d1916993681cdd94385
Michael Nowak <michael.nowak@sammatz.de>
2025-03-26 11:06:40 +0100

    Use sudo when we are not root

--- check_nginx_config.sh
+++ check_nginx_config.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-OUTPUT=$(nginx -t 2>&1 |tail --lines 1)
-RESULT=$?
+if [ $(whoami) = "root" ]; then
+	OUTPUT_TEST=$(nginx -t 2>&1)
+else
+	OUTPUT_TEST=$(sudo nginx -t 2>&1)
+fi
+RESULT_TEST=$?
+OUTPUT_CHECK=$(echo -e "$OUTPUT_TEST" |tail --lines 1)
 
-if [ $RESULT -eq 0 ]; then
-	echo "OK - $OUTPUT"
+if [ $RESULT_TEST -eq 0 ]; then
+	echo "OK - $OUTPUT_CHECK"
 	exit 0
 else
-	echo "CRITICAL - $OUTPUT"
+	echo "CRITICAL - $OUTPUT_CHECK"
 	exit 2
 fi
 
